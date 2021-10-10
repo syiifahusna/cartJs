928407df07f036a41faaf04ad7610634020700e3
syiifahusna <syiifahusna@gmail.com>
2021-10-11 00:58:20 +0800

    full function cart

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //============================= Add Into Cart Action =========================
 //element
 lblCart = document.getElementsByClassName("cart");
+
+//get totalCart from local storage
 var totalCart = parseInt(localStorage.getItem("totalCart"));
 //check if totalCart exist in local storage
 if(totalCart){
@@ -11,9 +13,8 @@ if(totalCart){
     lblCart[0].innerHTML = 0;
 }
 
-//button
+//btn add to cart action
 btnAddToCart = document.getElementsByClassName("btn-cart");
-
 for(let i=0;i<btnAddToCart.length;i++){
     btnAddToCart[i].addEventListener("click", function(){
         addToCart(i);
@@ -27,6 +28,30 @@ function addToCart(i){
     localStorage.setItem("totalCart",totalCart);
     lblCart[0].innerHTML = localStorage.getItem("totalCart");
     addProduct(i);
+
+
+    arrCart= [];
+    //check if itemInCart exist in local storage
+    itemInCart = localStorage.getItem("itamInCart");
+    if(itemInCart){
+        //if exist
+        //convert string into array
+        arrCart = itemInCart.split(',');
+        product = "product"+i;
+        //check if item exist in itemCart
+        if(arrCart.includes(product)){
+            //if true
+        }else{
+            //if false
+            arrCart.push("product"+i);
+            localStorage.setItem("itamInCart", arrCart);
+        }
+    }else{
+        //if not
+        //create the key
+        arrCart.push("product"+i);
+        localStorage.setItem("itamInCart", arrCart);
+    }
 }
 
 //add product into cart
@@ -49,6 +74,7 @@ function addProduct(i){
         productInCart = 1;
     }
 
+    //put into object
     var objProduct = {
         "productName": productName,
         "productPrice": productPrice,
@@ -61,74 +87,223 @@ function addProduct(i){
     localStorage.setItem("product"+i, objProductjson);
 }
 
-
 //============================= End Add Into Cart Action =========================
 
 //============================= Display In Cart Page =========================
-   
+displayCart();
+
+function displayCart(){
+    itemInCart = localStorage.getItem("itamInCart");
+    if(itemInCart){
+        //if exist
+        //convert string into array
+        arrCart = itemInCart.split(',');
+
+        text ="";
+        total = 0;
+        payment = 0;
+        for(let i=0; i<arrCart.length;i++){
+            product = "product"+i;
+    
+            keyName = arrCart[i];
+
+            objProduct = JSON.parse(localStorage.getItem(keyName));
+
+            price =parseFloat(objProduct.productPrice);
+            productInCart = parseInt(objProduct.productInCart);
+    
+            total =  price * productInCart;
+    
+            payment = payment + total;
+    
+            text = text +
+            `
+            <div class="box" id="`+keyName+`">
+                <div class="product">
+                    <div class="product-info">
+                        <div class="name">`+objProduct.productName+`</div>
+                        <div>RM <span class="">`+objProduct.productPrice+`</span></div>
+                        <div class="delivery">Delivery: Free</div>
+                        <div class="total-price">Total Price: RM `+ total.toFixed(2) +`</div>
+                    </div>
+                    <div class="quantity">
+                        <button class="btn-decrement">-</button>
+                            <span class="item-quantity">`+ objProduct.productInCart+`</span>
+                        <button class="btn-increment">+</button>
+                    </div>                    
+                    <button class="btn-remove"><i class="fas fa-trash"></i>Remove</button>
+                </div>
+            </div>  
+            `
+           
+        }
+    
+        cartContainer = document.getElementsByClassName("cart-container");
+        cartContainer[0].innerHTML = text;
+    
+        cartContainer = document.getElementsByClassName("total-payment");
+        cartContainer[0].innerHTML = "Total Payment: RM "+ payment.toFixed(2);
+
+        removeFromCart();
+        addMinusItem();
+        
+    }else{
+        //if not
+        cartContainer = document.getElementsByClassName("cart-container");
+        cartContainer[0].innerHTML = "Cart Empty";
+
+        cartContainer = document.getElementsByClassName("total-payment");
+        cartContainer[0].innerHTML = "Total Payment: RM 0.00";
+    }
+}
+
 
 //============================= End Display In Cart Page =========================
 
 //============================= Btn Remove Action =========================
 
-btnRemove = document.getElementsByClassName("btn-remove");
-
-for(let i = 0; i<btnRemove.length; i++){
-    btnRemove[i].addEventListener("click",function(){
-        product = "product"+i;
-        //convert to javascript object
-        objProduct = JSON.parse(localStorage.getItem(product));
-        //get current item in cart
-        inCart = parseInt(objProduct.productInCart);
+function removeFromCart(){
+    btnRemove = document.getElementsByClassName("btn-remove");
+    for(let i = 0; i<btnRemove.length; i++){
+        btnRemove[i].addEventListener("click",function(){
+            keyName = arrCart[i];
+            //convert product to javascript object
+            objProduct = JSON.parse(localStorage.getItem(keyName));
+            //get current item in cart
+            inCart = parseInt(objProduct.productInCart);
         
-        //remove in cart from total cart
-        totalCart = totalCart - inCart;
-        //update cart inner html
-        lblCart[0].innerHTML = totalCart;
+            //remove in cart from total cart
+            totalCart = totalCart - inCart;
+            //update cart inner html
+            lblCart[0].innerHTML = totalCart;
 
-        //set new totalcart
-        localStorage.setItem("totalCart",totalCart);
-        //remove product from local storage
-        localStorage.removeItem(product);
-        //remove html element
-        //get product id element
-        productId = document.getElementById(product);
-        productId.remove();
-    })
+            //set new totalcart
+            localStorage.setItem("totalCart",totalCart);
+            //remove product from local storage
+            localStorage.removeItem(keyName);
+
+
+            itemInCart = localStorage.getItem("itamInCart");
+
+            arrCart = itemInCart.split(',');
+            //remove item from arrCart
+            //get keyName location in arrCart
+            locationKeyName = arrCart.indexOf(keyName);
+            //at location keyname remove 1 item
+            arrCart.splice(locationKeyName, 1);
+
+            //set new set new itemInCart
+            localStorage.setItem("itamInCart",arrCart);
+
+            //remove html element
+            //get product id element
+            productId = document.getElementById(keyName);
+            productId.remove();
+
+            displayCart();
+            
+        })
+    }
 }
+
+
 //============================= End Btn Remove Action =========================
 
 //============================= Btn Quantity Action =========================
-//get element
-var quantity = document.getElementsByClassName("quantity");
-var itemQuantity = document.getElementsByClassName("item-quantity");
 
-//button
-var btnDecrement = document.getElementsByClassName("btn-decrement");
-var btnIncrement = document.getElementsByClassName("btn-increment");
 
-for(let i = 0; i<quantity.length; i++){
-    btnDecrement[i].addEventListener("click", function(){
-        item = parseInt(itemQuantity[i].innerHTML);
-        if(item <= 1){
-            alert("Item Reached Limit");
-            item = 1;
-        }else{
-            item = item - 1;
-        }
-        itemQuantity[i].innerHTML = item;
-    });
+function addMinusItem(){
 
-    btnIncrement[i].addEventListener("click", function(){
-        item = parseInt(itemQuantity[i].innerHTML);
-        if(item >= 10){
-            alert("Item Reached Limit");
-            item =10;
-        }else{
-            item = item + 1;
-        }
-        
-        itemQuantity[i].innerHTML = item;
-    });
+    //get element
+    var quantity = document.getElementsByClassName("quantity");
+    var itemQuantity = document.getElementsByClassName("item-quantity");
+
+    //button
+    var btnDecrement = document.getElementsByClassName("btn-decrement");
+    var btnIncrement = document.getElementsByClassName("btn-increment");
+
+
+    for(let i = 0; i<quantity.length; i++){
+
+        btnDecrement[i].addEventListener("click", function(){
+            item = parseInt(itemQuantity[i].innerHTML);
+
+             //update in totalCart local storage
+             totalCart = parseInt(localStorage.getItem("totalCart"));
+             newTotalCart = 0;
+             if(item <= 1){
+                 newTotalCart = totalCart;
+                 item = 1;
+             }else{
+                 newTotalCart = totalCart - 1;
+                 item = item - 1;
+             }
+
+             localStorage.setItem("totalCart",newTotalCart);
+ 
+             lblCart = document.getElementsByClassName("cart");
+             lblCart.innerHTML = localStorage.getItem("totalCart");
+            
+            itemQuantity[i].innerHTML = item;
+            currentQuantity = itemQuantity[i].innerHTML;      
+            
+            //get itemincart value
+            itemInCart = localStorage.getItem("itamInCart");
+            //put into array 
+            arrCart = itemInCart.split(',');
+            keyName = arrCart[i];
+
+            //update in product local storage
+            //get product from local storage and convert to javascript obj
+            objProduct = JSON.parse(localStorage.getItem(keyName));
+            objProduct.productInCart = currentQuantity;
+            const objProductjson = JSON.stringify(objProduct);
+            localStorage.setItem(keyName, objProductjson);
+
+            displayCart();
+        });
+
+        btnIncrement[i].addEventListener("click", function(){
+
+            item = parseInt(itemQuantity[i].innerHTML);
+
+             //update in totalCart local storage
+             totalCart = parseInt(localStorage.getItem("totalCart"));
+             newTotalCart = 0;
+            if(item >= 10){
+                alert("Item Reached Limit");
+                newTotalCart = totalCart;
+                item =10;
+            }else{
+                newTotalCart = totalCart + 1;
+                item = item + 1;
+            }
+
+            localStorage.setItem("totalCart",newTotalCart);
+ 
+            lblCart = document.getElementsByClassName("cart");
+            lblCart.innerHTML = localStorage.getItem("totalCart");
+
+            itemQuantity[i].innerHTML = item;
+            currentQuantity = itemQuantity[i].innerHTML;      
+            
+            //get itemincart value
+            itemInCart = localStorage.getItem("itamInCart");
+            //put into array 
+            arrCart = itemInCart.split(',');
+            keyName = arrCart[i];
+
+            //update in product local storage
+            //get product from local storage and convert to javascript obj
+            objProduct = JSON.parse(localStorage.getItem(keyName));
+            objProduct.productInCart = currentQuantity;
+            const objProductjson = JSON.stringify(objProduct);
+            localStorage.setItem(keyName, objProductjson);
+
+            displayCart();
+        });
+    }
+
 }
+
 //============================= End Btn Quantity Action =========================
